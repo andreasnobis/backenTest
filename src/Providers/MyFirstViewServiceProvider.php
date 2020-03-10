294288d29cba54b02c078e5d06be0e46765ae45c
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-10 16:30:11 +0100

    removed Function from serviceprovider

--- src/Providers/MyFirstViewServiceProvider.php
+++ src/Providers/MyFirstViewServiceProvider.php
@@ -12,18 +12,7 @@ use Plenty\Plugin\ServiceProvider;
 
         }
         
-        // access configuration from PHP
-        function getTitle(ConfigRepository $config):string
-        {
-            if( $config->get('MyPlugin.show_title') == "1" )
-              {
-                  return $config->get('MyPlugin.title_text');
-              }
-            else
-              {
-                  return "No Title in Config";
-              }
-        }
+       
         
     }
 
