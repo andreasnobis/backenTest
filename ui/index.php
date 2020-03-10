0c47bf8bd78e188715d03c0100efb9ceea3a2251
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-10 16:31:43 +0100

    added index.php

---
+++ ui/index.php
@@ -0,0 +1,14 @@
+<?php
+
+ // access configuration from PHP
+        function getTitle(ConfigRepository $config):string
+        {
+            if( $config->get('MyPlugin.show_title') == "1" )
+              {
+                  return $config->get('MyPlugin.title_text');
+              }
+            else
+              {
+                  return "No Title in Config";
+              }
+        }
