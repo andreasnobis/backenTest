8417e29d96a4f99796e909817ef3f16c01047adb
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-10 15:56:54 +0100

    Added PhPClass.php to providers

---
+++ src/Providers/PhpClass.php
@@ -0,0 +1,12 @@
+// access configuration from PHP
+function getTitle(ConfigRepository $config):string
+  {
+      if( $config->get('MyPlugin.show_title') == "1" )
+          {
+              return $config->get('MyPlugin.title_text');
+                  }
+                      else
+                  {
+              return "";
+          }
+  }
