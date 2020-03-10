2b155161463dea3d11bf6f3b352f6e268296a8ad
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-10 16:06:11 +0100

    added Function to ServiceProvider Class

--- src/Providers/MyFirstViewServiceProvider.php
+++ src/Providers/MyFirstViewServiceProvider.php
@@ -11,4 +11,19 @@ use Plenty\Plugin\ServiceProvider;
         {
 
         }
+        
+        // access configuration from PHP
+        function getTitle(ConfigRepository $config):string
+        {
+            if( $config->get('MyPlugin.show_title') == "1" )
+              {
+                  return $config->get('MyPlugin.title_text');
+              }
+            else
+              {
+                  return "No Title in Config";
+              }
+        }
+        
     }
+
