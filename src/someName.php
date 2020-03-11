130cec7f5feeafcd65e965d9f0c331384e70de4c
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-11 10:52:00 +0100

    index.php renamed and moved to src

---
+++ src/someName.php
@@ -0,0 +1,18 @@
+<?php
+echo'
+<h1>Hello World!</h1>
+Test Meta
+<meta http-equiv="refresh" content="20; URL=https://2.it-stnn.de/programme/CheckOrder/">
+    {% if config('MyPlugin.show_title') == "1" %}
+        <h1>IF{{ config('MyPlugin.title_text') }}</h1>
+    {% else %}
+        <h1>ELSE{{ config('MyPlugin.getTitle') }}</h1>
+    {% endif %}
+Test Nach Twig
+<!--
+<img src="../images/plentymarkets_graffiti.gif">
+Test Anfang5
+<a href="https://2.it-stnn.de/programme/CheckOrder/">Angular</a>
+<iframe src="https://2.it-stnn.de/programme/CheckOrder"/>
+ //-->
+ ';
