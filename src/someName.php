d2098b4a32dd034ca945892f469f084199683a1c
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-11 11:05:40 +0100

    very simple test

--- src/someName.php
+++ src/someName.php
@@ -1,6 +1,8 @@
 <?php
-echo"
-<h1>Hello World!</h1>
+echo"<h1>Hello World!</h1>";
+
+/*
+Auskommentiert:
 Test Meta
 <meta http-equiv='refresh' content='20; URL=https://2.it-stnn.de/programme/CheckOrder/'>
     {% if config('MyPlugin.show_title') == '1' %}
@@ -15,4 +17,5 @@ Test Anfang5
 <a href='https://2.it-stnn.de/programme/CheckOrder/'>Angular</a>
 <iframe src='https://2.it-stnn.de/programme/CheckOrder'/>
  //-->
- ";
+
+*/
