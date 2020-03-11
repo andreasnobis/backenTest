b5b5a91d23462925bcd1e4da9f660aec164d3d1d
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-11 08:11:16 +0100

    extend service provider

--- src/Providers/MyFirstViewServiceProvider.php
+++ src/Providers/MyFirstViewServiceProvider.php
@@ -9,6 +9,18 @@ use Plenty\Plugin\ServiceProvider;
         public function register()
         {
 
-        }       
+        }   
+        // access configuration from PHP
+        public function getTitle(ConfigRepository $config):string
+        {
+            if( $config->get('MyPlugin.show_title') == "1" )
+              {
+                  return $config->get('MyPlugin.title_text');
+              }
+            else
+              {
+                  return "No Title in Config";
+              }
+        }
     }
 
