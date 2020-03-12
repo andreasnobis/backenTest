4df1fd965d513e66584331540a67b76872073859
andreasnobis <andreas.nobis@spreen-online.de>
2020-03-12 09:53:58 +0100

    put in ui

---
+++ ui/userClass.php
@@ -0,0 +1,17 @@
+<?php
+namespace MyFirstViewTest\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+// access configuration from PHP
+    function getTitle(ConfigRepository $config):string
+        {
+            if( $config->get('MyPlugin.show_title') == "1" )
+                {
+                    return $config->get('MyPlugin.title_text');
+                        }
+                            else
+                        {
+                    return "No Result";
+                }
+        }
